53311bc1e77d886fa538b70006c148efa87d5050
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-29 18:20:10 +0900

    add unit tests

--- src/unit_test.rs
+++ src/unit_test.rs
@@ -16,3 +16,44 @@ fn double_value(a: i32) -> i32 {
 fn greeting(name: &str) -> String {
   format!("Hello {} san", name)
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn test_a_is_larger() {
+    let a = Rectangle {
+      width: 5,
+      height: 5,
+    };
+    let b = Rectangle {
+      width: 3,
+      height: 3,
+    };
+    assert!(a.compare_area(&b));
+  }
+
+  #[test]
+  fn test_a_is_smaller() {
+    let a = Rectangle {
+      width: 3,
+      height: 3,
+    };
+    let b = Rectangle {
+      width: 5,
+      height: 5,
+    };
+    assert!(!(a.compare_area(&b)));
+  }
+
+  #[test]
+  fn test_double() {
+    assert_eq!(6, double_value(3));
+  }
+
+  #[test]
+  fn test_contains_name() {
+    let res = greeting("rust");
+    assert!(res.contains("rust"));
+  }
+}
